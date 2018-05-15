218933068c92aa6cd4471bc0d593e8d5aab92bd3
Max Horn <max@quendi.de>
2018-05-15 12:04:37 +0200

    Change jl_gc_wb_back to jl_gc_wb in ResizeBag

--- src/julia_gc.c
+++ src/julia_gc.c
@@ -634,7 +634,7 @@ UInt ResizeBag(Bag bag, UInt new_size)
 
         // update the master pointer
         SET_PTR_BAG(bag, DATA(header));
-        jl_gc_wb_back((void *)bag);
+        jl_gc_wb((void *)bag, header);
     }
 
     // update the size
